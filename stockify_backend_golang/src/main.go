e3e9a3db7f0b2677086c51070181c23e96efb64f
NextPage02 <moshiur.r.nafi@gmail.com>
2024-12-15 17:55:09 +0600

    added controllers for crud operations for both items and users

--- stockify_backend_golang/src/main.go
+++ stockify_backend_golang/src/main.go
@@ -18,27 +18,27 @@ var userService = userservice.UserServiceImplementation(userRepository)
 func main() {
 }
 
-func addUser(user usermodel.User) {
+func AddUser(user usermodel.User) {
 	userService.AddUser(user)
 }
 
-func getAllUsers() []usermodel.User {
+func GetAllUsers() []usermodel.User {
 	return userService.GetAllUsers()
 }
 
-func getUserById(id uint64) usermodel.User {
+func GetUserById(id uint64) usermodel.User {
 	return userService.GetUserById(id)
 }
 
-func updateUser(updatedUser usermodel.User) {
+func UpdateUser(updatedUser usermodel.User) {
 	userService.UpdateUser(updatedUser)
 }
 
-func deleteUserById(id uint64) {
+func DeleteUserById(id uint64) {
 	userService.DeleteUserById(id)
 }
 
-func getFilteredUsers(params usermodel.UserQueryParams) []usermodel.User {
+func GetFilteredUsers(params usermodel.UserQueryParams) []usermodel.User {
 	users, err := userService.GetFilteredUsers(params)
 	if err != nil {
 		log.Fatal(err)
@@ -46,26 +46,26 @@ func getFilteredUsers(params usermodel.UserQueryParams) []usermodel.User {
 	return users
 }
 
-func addItem(item itemmodel.Item) {
+func AddItem(item itemmodel.Item) {
 	itemService.AddItem(item)
 }
 
-func getAllItems() []itemmodel.Item {
+func GetAllItems() []itemmodel.Item {
 	return itemService.GetAllItems()
 }
 
-func getItemById(id uint64) itemmodel.Item {
+func GetItemById(id uint64) itemmodel.Item {
 	return itemService.GetItemById(id)
 }
 
-func updateItem(updatedItem itemmodel.Item) {
+func UpdateItem(updatedItem itemmodel.Item) {
 	itemService.UpdateItem(updatedItem)
 }
-func deleteItem() {
+func DeleteItem() {
 	itemService.DeleteItemById(1)
 }
 
-func getFilteredItems(params itemmodel.ItemFilterParams) []itemmodel.Item {
+func GetFilteredItems(params itemmodel.ItemFilterParams) []itemmodel.Item {
 	items, err := itemService.GetFilteredItems(params)
 	if err != nil {
 		log.Fatal(err)
