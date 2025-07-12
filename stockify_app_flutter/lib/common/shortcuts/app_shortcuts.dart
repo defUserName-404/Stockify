92c76a0381aa1e7b10ac67296c0b57b10c00f7e7
NextPage02 <moshiur.r.nafi@gmail.com>
2025-07-12 17:10:54 +0600

    added list shortcut on settings

--- stockify_app_flutter/lib/common/shortcuts/app_shortcuts.dart
+++ stockify_app_flutter/lib/common/shortcuts/app_shortcuts.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 
 class AppShortcuts {
   // General
@@ -29,19 +29,42 @@ class AppShortcuts {
 class Shortcut {
   final String description;
   final String combination;
+  final IconData icon;
 
-  Shortcut({required this.description, required this.combination});
+  Shortcut({
+    required this.description,
+    required this.combination,
+    this.icon = Icons.keyboard,
+  });
 }
 
 final List<Shortcut> shortcutList = [
-  Shortcut(description: 'Open Search', combination: 'Ctrl + F'),
-  Shortcut(description: 'Open Filter', combination: 'Ctrl + Shift + F'),
-  Shortcut(description: 'Add New Item/User', combination: 'Ctrl + N'),
+  Shortcut(
+      description: 'Open Search', combination: 'Ctrl + F', icon: Icons.search),
+  Shortcut(
+      description: 'Open Filter',
+      combination: 'Ctrl + Shift + F',
+      icon: Icons.filter_list),
+  Shortcut(
+      description: 'Add New Item/User',
+      combination: 'Ctrl + N',
+      icon: Icons.add),
   // Shortcut(description: 'Next Page', combination: 'Ctrl + Right Arrow'),
   // Shortcut(description: 'Previous Page', combination: 'Ctrl + Left Arrow'),
-  Shortcut(description: 'Go to Dashboard', combination: 'Ctrl + 1'),
-  Shortcut(description: 'Go to Items', combination: 'Ctrl + 2'),
-  Shortcut(description: 'Go to Users', combination: 'Ctrl + 3'),
-  Shortcut(description: 'Go to Notifications', combination: 'Ctrl + 4'),
-  Shortcut(description: 'Go to Settings', combination: 'Ctrl + 5'),
+  Shortcut(
+      description: 'Go to Dashboard',
+      combination: 'Ctrl + 1',
+      icon: Icons.dashboard),
+  Shortcut(
+      description: 'Go to Items', combination: 'Ctrl + 2', icon: Icons.list),
+  Shortcut(
+      description: 'Go to Users', combination: 'Ctrl + 3', icon: Icons.people),
+  Shortcut(
+      description: 'Go to Notifications',
+      combination: 'Ctrl + 4',
+      icon: Icons.notifications),
+  Shortcut(
+      description: 'Go to Settings',
+      combination: 'Ctrl + 5',
+      icon: Icons.settings),
 ];
